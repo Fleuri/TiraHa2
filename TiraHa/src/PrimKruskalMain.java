8b4ef0c9655ed40e7a9742db8b1f3c30f565b62c
Lauri Suomalainen <>
2012-05-21 21:38:11 +0300

    Teko "aloitettu"

---
+++ TiraHa/src/PrimKruskalMain.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Acer
+ */
+public class PrimKruskalMain {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    }
+}
