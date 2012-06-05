63efb94bd1bc845b3e35ba2475ea402a501c36f8
Lauri Suomalainen <>
2012-06-05 21:27:38 +0300

    Hajautustaulua alettu tehdä.

---
+++ TiraHa/src/Hajautusapu.java
@@ -0,0 +1,31 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Lauri Suomalainen
+ */
+public class Hajautusapu {
+    public Solmu solmu;
+    public int pituus;
+    public boolean isEmpty;
+    
+    public Hajautusapu(Solmu solmu, int pituus) {
+        this.solmu = solmu;
+        this.pituus = pituus;
+        isEmpty = false;
+    }
+
+    public boolean isEmpty() {
+        return isEmpty();
+    }
+    public void changeisEmpty() {
+        if (isEmpty == true) {
+            isEmpty = false;
+        } else {
+            isEmpty = true;
+        }
+    }
+}
---
+++ TiraHa/src/Hajautustaulu.java
@@ -0,0 +1,55 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Lauri Suomalainen
+ */
+public class Hajautustaulu {
+    Hajautusapu[] lista;
+    
+    public Hajautustaulu(){
+        lista = new Hajautusapu[11];
+        }
+    public boolean insert(Solmu solmu, int pituus){
+        int i = 0;
+        int summa = solmu.hashCode();
+        while (true) {
+        int sijoitus = (i + summa) % lista.length;
+            if (lista[sijoitus] == null || lista[sijoitus].isEmpty()) {
+                lista[sijoitus] = new Hajautusapu(solmu, pituus);
+                return true;
+            } i++;
+            if (i == lista.length)
+                pidennalista();
+        }
+    }
+
+    private void pidennalista() {
+        while (true) {
+        int i = lista.length;
+            if (isPrime(i)) {
+                Hajautusapu[] lista2 = lista;
+                lista = new Hajautusapu[i];
+                for (int j = 0; j < lista2.length; j++) {
+                    if (lista2[j] != null) {
+                        insert(lista2[j].solmu, lista2[j].pituus); 
+                    }
+                } return;
+            }
+            i++;
+    }
+    }
+
+    private boolean isPrime(int i) {
+        while (true) {
+            for (int j = 2; j < i; j++) {
+                if (i % j != 0) {
+                    return true;
+                }
+            }
+        }
+    }
+}
